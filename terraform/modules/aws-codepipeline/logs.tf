3f2cb02a526bbc032df4b148b1cc54073804594f
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2024-11-27 14:40:40 +0000

    Updated log groups to be prefixed with the pipeline name.

--- terraform/modules/aws-codepipeline/logs.tf
+++ terraform/modules/aws-codepipeline/logs.tf
@@ -1,10 +1,10 @@
 resource "aws_cloudwatch_log_group" "plan" {
-  name              = "/aws/codebuild/terraform-plan"
+  name              = "/aws/codebuild/${var.pipeline_name}-terraform-plan"
   retention_in_days = "1"
 }
 
 resource "aws_cloudwatch_log_group" "apply" {
-  name              = "/aws/codebuild/terraform-apply"
+  name              = "/aws/codebuild/${var.pipeline_name}-terraform-apply"
   retention_in_days = "1"
 }
 
