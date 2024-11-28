3432ff6bc5da38591f6379bb043fda42b4bd5941
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2024-11-28 20:25:21 +0000

    Removing provider from common module.

--- terraform/modules/common/providers.tf
+++ terraform/modules/common/providers.tf
@@ -8,13 +8,13 @@ terraform {
   required_version = "~> 1.9.6"
 }
 
-provider "aws" {
-  region = "us-east-1"
-  default_tags {
-    tags = {
-      Environment = "Development"
-      DeployedBy  = "Terraform"
-      CodeRepo    = "aws-master-pipeline"
-    }
-  }
-}
+//provider "aws" {
+//  region = "us-east-1"
+//  default_tags {
+//    tags = {
+//      Environment = "Development"
+//      DeployedBy  = "Terraform"
+//      CodeRepo    = "aws-master-pipeline"
+//    }
+//  }
+//}
