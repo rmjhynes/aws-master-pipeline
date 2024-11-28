7caf67e21f1ca23467cb3e07fd98be81174f3e2f
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2024-11-28 17:45:09 +0000

    Sourcing the bucket info rather than using a module.

--- terraform/pipelines-to-deploy/main.tf
+++ terraform/pipelines-to-deploy/main.tf
@@ -2,6 +2,12 @@ module "common" {
   source = "../modules/common/"
 }
 
+data "aws_caller_identity" "current" {}
+
+data "aws_s3_bucket" "shared_pipeline_artifact_bucket" {
+  bucket = "shared-artifact-bucket-${data.aws_caller_identity.current.id}"
+}
+
 module "worker_pipelines" {
   source = "../modules/aws-codepipeline/"
 
@@ -10,7 +16,9 @@ module "worker_pipelines" {
   account_id    = each.value.deployment_accounts[0]
   repository_id = each.value.code_repository
   shared_pipeline_artifact_bucket = {
-    id  = module.common.shared_pipeline_artifact_bucket_id
-    arn = module.common.shared_pipeline_artifact_bucket_arn
+    //id  = module.common.shared_pipeline_artifact_bucket_id
+    id = data.aws_s3_bucket.shared_pipeline_artifact_bucket.id
+    //arn = module.common.shared_pipeline_artifact_bucket_arn
+    arn = data.aws_s3_bucket.shared_pipeline_artifact_bucket.arn
   }
 }
