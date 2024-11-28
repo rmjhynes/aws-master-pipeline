154c98f50f10a8dbdcfcd1600d581f0d089ecea3
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2024-11-28 20:00:36 +0000

    Testing s3 backend.

--- terraform/modules/aws-codepipeline/providers.tf
+++ terraform/modules/aws-codepipeline/providers.tf
@@ -1,8 +1,8 @@
 terraform {
-  backend "s3" {
-    bucket = "terraform-state-126781719022"
-    region = "us-east-1"
-  }
+  //  backend "s3" {
+  //    bucket = "terraform-state-126781719022"
+  //    region = "us-east-1"
+  //  }
 
   required_providers {
     aws = {
